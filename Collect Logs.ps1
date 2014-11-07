ec0cdf69e0c9788099ce623ef96c8cdea895b4ed
itadder <justinogarcia@justinogarciaconsulting.com>
2014-11-07 08:09:42 -0800

    Collect Office Logs
    Collect Office Logs to troubleshoot.

---
+++ Collect Logs.ps1
@@ -0,0 +1,33 @@
+﻿ cls
+function Get-OfficeErrorLogs
+{
+ param (  
+   
+    $computername = 'localhost',
+    $dayrange = 1
+    )
+
+    #get envent logs
+    $officeerrorlogs = Get-EventLog -ComputerName $computername -LogName Application -Source "Application Error" -After ( get-date). AddDays(-$dayrange ) | Select-Object Message
+
+    $obj = New-Object -TypeName psobject
+
+    $obj | Add-Member -MemberType NoteProperty `
+        -name ComputerName -Value $computername
+   
+    $obj | Add-Member -MemberType NoteProperty `
+        -name Days -value $dayrange
+
+    $obj | Add-Member -MemberType NoteProperty `
+        -name Message -Value $officeerrorlogs   `
+
+
+       Write-Output $obj
+      
+
+
+   
+}
+
+Get-OfficeErrorLogs -computername localhost -dayrange 6 | Format-Table -AutoSize
+ 
